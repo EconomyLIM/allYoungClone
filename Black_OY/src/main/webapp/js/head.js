b00dff197d8c1b09de1f28cc76293a3d4b871d8a
pilot0311 <pilot0938@gmail.com>
2023-11-03 17:29:29 +0900

    head-script 수정

--- Black_OY/src/main/webapp/js/head.js
+++ Black_OY/src/main/webapp/js/head.js
@@ -1,11 +1,27 @@
 
 window.onload = () =>{
 	document.getElementById("btnGnbOpen").onclick = function () {
-    document.querySelector(".layer_all_menu").className += " active";
+    if(document.querySelector(".layer_all_menu").classList.contains("active")){
+                document.querySelector(".layer_all_menu.active").className = "layer_all_menu";
+            }else{
+                document.querySelector(".layer_all_menu").className += " active";
+            }
 }
 
 document.getElementById("btnGnbClose").onclick = function () {
     document.querySelector(".layer_all_menu.active").className = "layer_all_menu";
 }
 
+
+$(".mymenu_layer").click(function(){
+            $("li.recent").addClass("open");
+            
+        })
+        $("li.recent").mouseleave(function(){
+            $("li.recent").removeClass("open");
+            
+        })
+
 };
+
+
