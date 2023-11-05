2eb1b1ddc2334008139513da487822c792e02919
LeeSangMun <zbqm1919@naver.com>
2023-11-05 14:56:13 +0900

    드디어 프론트 첫 기능

--- Black_OY/src/main/webapp/js/mainPage.js
+++ Black_OY/src/main/webapp/js/mainPage.js
@@ -5,26 +5,55 @@
 /***  main full banner ***/
 
 let timer;
+let flag = false; // main full banner의 timer 조건
 
 $(function() {
-	setInterval(mainFullSlider, 5000);
+	timer = setInterval(mainFullSlider, 5000);	
+	
+	$("#mainSliderBtn").on("click", function() {
+		$(this).toggleClass("pause");
+		if(flag) {
+			timer = setInterval(mainFullSlider, 5000);		
+		} else {
+			clearInterval(timer);
+		}
+		flag = !flag;
+	});
+	
+	$("#mainFullSlider .slick-next").on("click", function() {
+		mainFullSlider();
+	});
+	
+	$("#mainFullSlider .slick-prev").on("click", function() {
+		mainFullSlider(-1);
+	});
 });
 
-function mainFullSlider() {
-	let banners = $("#mainFullSlider .slick-list .slick-track .slick-slide").length;
-	let slick_index = $("#mainFullSlider .slick-list .slick-track .slick-slide.slick-active").data("slick-index");
-	let index = (slick_index+1)%banners;
-	$("#mainFullSlider .slick-list .slick-track .slick-slide.slick-active")
+// 매개변수를 안주면 현재페이지+1
+// 매개변수 -1를 주면 현재페이지에 -1
+function mainFullSlider(addIndex = 1) {
+	let banners = $("#mainFullSlider .slick-slide").length;
+	let slick_index = $("#mainFullSlider .slick-slide.slick-active").data("slick-index");
+	let index = slick_index + addIndex;
+	if(index < 0) {
+		index = banners-1;
+	} else if(index > banners-1) {
+		index = 0;
+	}
+	
+	$("#mainFullSlider .slick-slide.slick-active")
 		.removeClass(["slick-active", "slick-current"])
 		.css({
 			"z-index" : "1"
 			, "opacity" : "0"
 		});
 	
-	$(`#mainFullSlider .slick-list .slick-track .slick-slide[data-slick-index=${index}]`)
+	$(`#mainFullSlider .slick-slide[data-slick-index=${index}]`)
 		.addClass(["slick-active", "slick-current"])
 		.css({
 			"z-index" : "999"
 			, "opacity" : "1"
 		});
+		
+	$("#mainFullSlider .slick-paging .cur_no").html(`${index+1}`);
 }
