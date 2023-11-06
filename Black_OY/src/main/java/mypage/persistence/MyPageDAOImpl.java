ecfdad381558a829fa11c2b231064346b0becd6b
apcat <alphacat0941@gmail.com>
2023-11-06 17:34:50 +0900

    mypage main dao,dto 추가

--- Black_OY/src/main/java/mypage/persistence/MyPageDAOImpl.java
+++ Black_OY/src/main/java/mypage/persistence/MyPageDAOImpl.java
@@ -1,7 +1,12 @@
 package mypage.persistence;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.util.JDBCUtil;
 
 import mypage.domain.MpPAskDTO;
 import mypage.domain.MpPlikeDTO;
@@ -9,13 +14,49 @@ import mypage.domain.MpQnADTO;
 import mypage.domain.MpUserInfoDTO;
 
 public class MyPageDAOImpl implements MypageDAO {
-
+	
+	private MyPageDAOImpl() {}
+	private static MyPageDAOImpl instance = new MyPageDAOImpl();
+	private static MyPageDAOImpl getinstance() {
+		return instance;
+	}
+	
+	
 	@Override
 	public List<MpUserInfoDTO> selectUserInfo(Connection conn, String Uid) throws Exception {
 		// TODO Auto-generated method stub
+		String sql = " SELECT U_NAME, GRADE_ID "
+				+ " FROM O_user "
+				+ " WHERE user_id = '?' ";
+		
+		ArrayList<MpUserInfoDTO> list = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, Uid);
+		rs = pstmt.executeQuery();
+		MpUserInfoDTO dto = null;
+		if (rs.next()) {
+			list = new ArrayList<MpUserInfoDTO>();
+			
+			do {
+				dto = new MpUserInfoDTO();
+				dto.setUName(rs.getString("uName"));
+				dto.setUGrade(rs.getString("uGrade"));
+				list.add(dto);
+			} while (rs.next());
+			JDBCUtil.close(pstmt);
+			JDBCUtil.close(rs);
+		}
+		
+		} catch (Exception e) {
+			System.out.println("> MyPageDAOImpl_selectUserInfo() Exception");
+		}
 		
 		
-		return null;
+		return list;
 	}
 
 	@Override
