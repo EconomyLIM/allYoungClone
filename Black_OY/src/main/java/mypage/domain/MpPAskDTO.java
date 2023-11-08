630d4b9af5be0731875676fa6f12c3f2d57ac474
apcat <alphacat0941@gmail.com>
2023-11-08 12:38:16 +0900

    Merge branch 'develop' of https://github.com/Team3-JSP/OliveYoung.git into feature_user

--- Black_OY/src/main/java/mypage/domain/MpPAskDTO.java
+++ Black_OY/src/main/java/mypage/domain/MpPAskDTO.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MpPAskDTO {
 	
-	private String pAskState; 		//1:1문의 상태
-	private String pAskQuestion;	//1:1문의 질문
-	private Date pAskDate;			//1:1문의 작성일
+	private String paskState; 		//1:1문의 상태
+	private String paskQuestion;	//1:1문의 질문
+	private Date paskDate;			//1:1문의 작성일
 }
--- Black_OY/src/main/java/mypage/persistence/MyPageDAOImpl.java
+++ Black_OY/src/main/java/mypage/persistence/MyPageDAOImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import com.util.JDBCUtil;
 
+import mypage.domain.MpOrderDTO;
 import mypage.domain.MpPAskDTO;
 import mypage.domain.MpPlikeDTO;
 import mypage.domain.MpQnADTO;
@@ -26,9 +27,9 @@ public class MyPageDAOImpl implements MypageDAO {
 	@Override
 	public List<MpUserInfoDTO> selectUserInfo(Connection conn, String Uid) throws Exception {
 		// TODO Auto-generated method stub
-		String sql = " SELECT u_name, gr_name "
-				+ "FROM O_user u left join olive_members m on u.grade_id = m.grade_id "
-				+ " WHERE user_id = '?' ";
+		String sql = " SELECT gr_name, u_name  "
+				+ " FROM O_user u left join olive_members m on u.grade_id = m.grade_id "
+				+ " WHERE user_id = ? ";
 		ArrayList<MpUserInfoDTO> list = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -43,8 +44,8 @@ public class MyPageDAOImpl implements MypageDAO {
 			
 			do {
 				dto = new MpUserInfoDTO();
-				dto.setUGrade("grade_id");
-				dto.setUName("u_name");
+				dto.setUgrade(rs.getString("gr_name"));
+				dto.setUname(rs.getString("u_name"));
 				list.add(dto);
 			} while (rs.next());
 			JDBCUtil.close(pstmt);
@@ -65,7 +66,7 @@ public class MyPageDAOImpl implements MypageDAO {
 		int UserPoint = 0;
 		String sql = " SELECT U_POINT "
 				+ " FROM O_user "
-				+ " WHERE user_id = '?' ";
+				+ " WHERE user_id = ? ";
 		
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -76,7 +77,7 @@ public class MyPageDAOImpl implements MypageDAO {
 			
 			if (rs.next()) {
 				UserPoint = rs.getInt("U_POINT");
-			}
+			} 
 		
 		}finally {
 			JDBCUtil.close(pstmt);
@@ -93,7 +94,7 @@ public class MyPageDAOImpl implements MypageDAO {
 		
 		String sql = " SELECT COUNT(*) c "
 				+ " FROM USER_COUPON "
-				+ " WHERE USER_ID = '?' ";
+				+ " WHERE USER_ID = ? ";
 		
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -104,7 +105,7 @@ public class MyPageDAOImpl implements MypageDAO {
 			
 			if (rs.next()) {
 				UserCouponCount = rs.getInt("c");
-			}
+			} 
 		
 		}finally {
 			JDBCUtil.close(pstmt);
@@ -120,7 +121,7 @@ public class MyPageDAOImpl implements MypageDAO {
 		
 		String sql = " SELECT t_des "
 				+ " FROM u_deposit "
-				+ " WHERE USER_ID = '?' ";
+				+ " WHERE USER_ID = ? ";
 		
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -131,7 +132,7 @@ public class MyPageDAOImpl implements MypageDAO {
 			
 			if (rs.next()) {
 				UserDeposit = rs.getInt("t_des");
-			}
+			} 
 		
 		}finally {
 			JDBCUtil.close(pstmt);
@@ -150,7 +151,7 @@ public class MyPageDAOImpl implements MypageDAO {
 				+ "    LEFT JOIN pro_prom p ON l.pro_displ_id = p.pro_displ_id "
 				+ "    LEFT JOIN product_display d ON l.pro_displ_id = d.pro_displ_id "
 				+ "    LEFT JOIN pro_displ_img i ON l.pro_displ_id = i.pro_displ_id "
-				+ "    WHERE user_id = '?' "
+				+ "    WHERE user_id = ? "
 				+ "    ORDER BY pro_displ_src  "
 				+ " ) "
 				+ " WHERE ROWNUM = 1  ";
@@ -193,7 +194,7 @@ public class MyPageDAOImpl implements MypageDAO {
 		// TODO Auto-generated method stub
 		String sql = " SELECT pask_state, pask_content, pask_date "
 				+ " FROM PERSONAL_ASK "
-				+ " WHERE USER_ID = 'admin'; ";
+				+ " WHERE USER_ID = ? ";
 		
 		ArrayList<MpPAskDTO> list = null;
 		PreparedStatement pstmt = null;
@@ -209,9 +210,9 @@ public class MyPageDAOImpl implements MypageDAO {
 			
 			do {
 				dto = new MpPAskDTO();
-				dto.setPAskState(rs.getString("pask_state"));
-				dto.setPAskQuestion(rs.getString("pask_content"));
-				dto.setPAskDate(rs.getDate("pask_date"));
+				dto.setPaskQuestion(rs.getString("pask_state"));
+				dto.setPaskQuestion(rs.getString("pask_content"));
+				dto.setPaskDate(rs.getDate("pask_date"));
 				list.add(dto);			
 			} while (rs.next());
 			JDBCUtil.close(pstmt);
@@ -230,7 +231,7 @@ public class MyPageDAOImpl implements MypageDAO {
 		// TODO Auto-generated method stub
 		String sql = " SELECT qa_que, qa_ans, qa_date "
 				+ " FROM qanda "
-				+ " WHERE user_id = '?' ";
+				+ " WHERE user_id = ? ";
 		ArrayList<MpQnADTO> list = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -266,7 +267,7 @@ public class MyPageDAOImpl implements MypageDAO {
 		int UserRevCount = 0;
 		String sql = " SELECT count(*) c "
 				+ " FROM review "
-				+ " WHERE user_id = '?' ";
+				+ " WHERE user_id = ? ";
 		
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -277,13 +278,56 @@ public class MyPageDAOImpl implements MypageDAO {
 			
 			if (rs.next()) {
 				UserRevCount = rs.getInt("c");
-			}
+			} 
 		
 		}finally {
 			JDBCUtil.close(pstmt);
 			JDBCUtil.close(rs);
 		}		
 		return UserRevCount;
+	}
+
+
+	@Override
+	public List<MpOrderDTO> selectUserOrderStatus(Connection conn, String Uid) throws Exception {
+		// TODO Auto-generated method stub
+		String sql = " SELECT COUNT(CASE WHEN order_status='주문접수' THEN 1 END) AS uorderState1 , "
+				+ "        COUNT(CASE WHEN order_status='결제완료' THEN 1 END) AS uorderState2 , "
+				+ "        COUNT(CASE WHEN order_status='배송준비중' THEN 1 END) AS uorderState3 , "
+				+ "        COUNT(CASE WHEN order_status='배송중' THEN 1 END) AS uorderState4 , "
+				+ "        COUNT(CASE WHEN order_status='배송완료' THEN 1 END) AS uorderState5  "
+				+ " FROM o_user u LEFT JOIN o_order o ON u.user_id = o.user_id "
+				+ " WHERE u.user_id = ? AND "
+				+ "    order_date BETWEEN SYSDATE - (INTERVAL '1' MONTH) AND SYSDATE ";	//최근1개월
+		ArrayList<MpOrderDTO> list = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, Uid);
+			rs = pstmt.executeQuery();
+			MpOrderDTO dto = null;
+		if (rs.next()) {
+			list = new ArrayList<MpOrderDTO>();
+			
+			do {
+				dto = new MpOrderDTO();
+				dto.setUorderState1(rs.getInt("uorderState1"));
+				dto.setUorderState2(rs.getInt("uorderState2"));
+				dto.setUorderState3(rs.getInt("uorderState3"));
+				dto.setUorderState4(rs.getInt("uorderState4"));
+				dto.setUorderState5(rs.getInt("uorderState5"));
+				list.add(dto);
+			} while (rs.next());
+			JDBCUtil.close(pstmt);
+			JDBCUtil.close(rs);
+		}
+		
+		} catch (Exception e) {
+			System.out.println("> MyPageDAOImpl_selectUserOrderStatus() Exception");
+		}
+		return list;
 	}
 
 
