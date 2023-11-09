fd8dc2f3d40e6790933f987e7d0a4991cd396140
juyeong <jybee13@gmail.com>
2023-11-09 13:32:42 +0900

    # WARNING: head commit changed in the meantime
    commt

--- Black_OY/src/main/java/user/command/JoinHandler.java
+++ Black_OY/src/main/java/user/command/JoinHandler.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.util.ConnectionProvider;
 
@@ -12,24 +13,35 @@ import command.CommandHandler;
 import user.domain.OuserDTO;
 import user.persistence.OuserDAOImpl;
 
-
+/*
 public class JoinHandler implements CommandHandler{
 
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		 request.setCharacterEncoding("utf-8");
-		 
-		 String name = request.getParameter("name");
-		 String tel=  request.getParameter("tel");
-		 String birth = request.getParameter("birth");
-		 System.out.println(tel);
-		 
+		System.out.println("> Join.process...");
+		String method = request.getMethod(); // GET, POST
+
+		String refer = null;
+		HttpSession session = request.getSession();
+		
+		if(method.equals("GET")) {
+
+			refer = (String) session.getAttribute("refer");
+			System.out.println("요청URL:" + refer);
+
+			return "/view/join/join.jsp";
+		} else {
+
+		 String user_id = request.getParameter("user_id");
+		 String u_name = request.getParameter("u_name");
+		 String u_pwd = request.getParameter("u_pwd");
+		 String u_birth = request.getParameter("u_birth");
+		 String u_tel=  request.getParameter("u_tel");
+
 		 Connection conn = ConnectionProvider.getConnection();
 		 OuserDAOImpl dao = new OuserDAOImpl(conn);
 		 OuserDTO dto = new OuserDTO();
-			dto.setUser_id("user_id");
-			dto.setU_pwd("u_pwd");
-			dto.setU_email("u_email");
+
 			conn.close();
 			
 			request.setAttribute("dto", dto);
@@ -39,5 +51,5 @@ public class JoinHandler implements CommandHandler{
 			return location;
 	}
 }
-
+*/
 
